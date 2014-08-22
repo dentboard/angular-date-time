c3d915e5cfa059c23a0e0b7d2c4a13cba3677a11
Abhinav Raghunathan <abhinavr2121@gmail.com>
2014-08-22 09:45:42 -0400

    Started hours and minutes
    Still have a couple of bugs

--- directive.js
+++ directive.js
@@ -4,14 +4,29 @@ app.directive("timeInput", function() {
   return {
     restrict: "E",
     scope: {
-          stepHr: "@",
-          stepMin: "@"
+      stepHr: "@",
+      stepMin: "@"
     },
     template: "<select ng-model='hr' ng-options='a for a in hrOptions'></select><select ng-model='min' ng-options='b for b in minOptions'></select><select ng-model='ampm' ng-options='c for c in ampmOptions'></select>",
     link: function(scope, element, attrs) {
-          scope_timeinput = scope;
-          scope.ampmOptions = ['AM','PM'];
-          scope.ampm = 'AM';
+      //global variable to grab in console
+      scope_timeinput = scope;
+
+      //AM and PM array
+      scope.ampmOptions = ['AM', 'PM'];
+      scope.ampm = 'AM';
+
+      //start hours
+      scope.$watch('stepHr', function(stepHr) {
+        scope.hrOptions = _.range(1, 13, stepHr);
+      });
+      scope.hr = 1;
+
+      //start minutes
+      scope.$watch('stepMin', function(stepMin) {
+            scope.minOptions = _.range(0, 59, stepMin);
+      });
+      scope.min = 0;
     }
   };
 });
